2fb0c5f9e6cfc97139157721380b5da950ec8d59
MD <dhmedy@googlemail.com>
2022-11-10 17:44:30 +0100

    User Registration, Login + Data Validation + Token Creation

---
+++ util/dataValidator.js
@@ -0,0 +1,20 @@
+// https://www.npmjs.com/package/password-validator
+import passwordValidator from 'password-validator';
+
+// This function check the validation of password: 
+export const checkPasswordValidation = (password) => {
+    // Create a schema
+    const schema = new passwordValidator();
+    // Add properties to it
+    schema
+        .is().min(6)                                    // Minimum length 6
+        .is().max(100)                                  // Maximum length 100
+        .has().uppercase()                              // Must have uppercase letters
+        .has().lowercase()                              // Must have lowercase letters
+        .has().digits(2)                                // Must have at least 2 digits
+    //.has().not().spaces()                           // Should not have spaces
+    //.is().not().oneOf(['Passw0rd', 'Password123']);
+    console.log(schema.validate(password));
+    const validationResult = schema.validate(password);
+    return validationResult;
+}
---
+++ util/token.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken'
+export const createToken = (claims) => {
+    const token = jwt.sign(claims, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
+    return token
+}
+
+export const verifyToken = (token) => {
+
+    const result = jwt.verify(token, process.env.JWT_SECRET)
+    return result
+
+}
