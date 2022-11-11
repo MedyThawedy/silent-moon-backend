1a639097ff9aa48b58e3f10b9ae192bed7e3be7d
MD <dhmedy@googlemail.com>
2022-11-11 17:19:45 +0100

    fixing error

--- controller/musicController.js
+++ controller/musicController.js
@@ -3,5 +3,5 @@ import { getAllMusic } from '../services/musicService.js';
 
 export const getMusic = async (req, res) => {
     const findAllMusic = await getAllMusic()
-    res.status(200).json({ findAllMusic });
+    res.status(200).json(findAllMusic);
 }
--- services/musicService.js
+++ services/musicService.js
@@ -3,6 +3,6 @@ import { getDb } from "../util/db.js";
 export const getAllMusic = async () => {
     // Get a playlist
     const db = await getDb();
-    const findAllMusic = await db.collection('music').find()
+    const findAllMusic = await db.collection('music').find().toArray()
     return findAllMusic;
 }
